56424c2860390571b4a000428ee6ca777d435b03
Andrew N'dri <koffi.ndri@amalitech.org>
2021-12-06 02:00:16 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(() =>{
+    $('.hint-box').on('click', ()=>{
+      $('.hint').slideToggle(500);
+    });
+    $('.wrong-answer-one').on('click', ()=>{
+      $('.wrong-text-one').fadeOut('slow');
+      $('.frown').show();
+    });
+    $('.wrong-answer-two').on('click', ()=>{
+      $('.wrong-text-two').fadeOut('fast');
+      $('.frown').show();
+    });
+    $('.wrong-answer-three').on('click', ()=>{
+      $('.wrong-text-three').fadeOut('fast');
+      $('.frown').show();
+    });
+    $('.correct-answer').on('click',()=>{
+      $('.frown').hide();
+      $('.smiley').show();
+      $('.wrong-text-one').fadeOut('slow');
+      $('.wrong-text-two').fadeOut('slow');
+      $('.wrong-text-three').fadeOut('slow');
+    });
+  });
